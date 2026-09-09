fa4980743409aabef69606518f613686c6570e8c
gega.khavtasi@yahoo.com <gega.khavtasi@yahoo.com>
2026-09-09 10:01:29 +0000

    date_dimension model

---
+++ models/date_dimension.sql
@@ -0,0 +1,21 @@
+WITH CTE AS (
+    SELECT 
+         TO_TIMESTAMP(STARTED_AT) AS STARTED_AT,
+         DATE(TO_TIMESTAMP(STARTED_AT)) AS DATE_STARTED_AT,
+         HOUR(TO_TIMESTAMP(STARTED_AT)) AS HOUR_STARTED_AT,
+         CASE 
+            WHEN DAYNAME(TO_TIMESTAMP(STARTED_AT)) in ('Sat','Sun')
+            THEN 'Weekend'
+            ELSE 'Businessday'
+        END AS  DAY_TYPE,
+        CASE
+            WHEN MONTH(TO_TIMESTAMP(STARTED_AT)) in (12,1,2) THEN 'Winter'
+            WHEN MONTH(TO_TIMESTAMP(STARTED_AT)) in (3,4,5) THEN 'Spring'
+            WHEN MONTH(TO_TIMESTAMP(STARTED_AT)) in (6,7,8) THEN 'Summer'
+            ELSE 'Autumn'
+        END AS STATION_OF_YEAR
+    FROM {{ source('demo', 'bike') }}
+    WHERE STARTED_AT != 'started_at'
+)
+
+SELECT * FROM CTE
