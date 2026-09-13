ed90933080b47079612bb0ab4d69a7b4e87cf8f5
gega.khavtasi@yahoo.com <gega.khavtasi@yahoo.com>
2026-09-13 03:28:26 +0000

    sadads

---
+++ models/btc/btc_stage_outputs.sql
@@ -0,0 +1,15 @@
+{{ config(materialized='table') }}
+
+select
+tx.hash_key,
+tx.block_number,
+tx.block_timestamp,
+tx.is_coinbase,
+f.value:address::STRING as output_address,
+f.value:value::FLOAT as output_value
+
+from {{ ref('btc_stg')}} tx,
+
+LATERAL FLATTEN(input => outputs) f
+
+WHERE f.value:address is not null
